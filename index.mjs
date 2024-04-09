67f1936ba35506e5ba55bc0f995b7d4d816ec32c
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2024-04-08 21:51:02 -0500

    adde-api_route

--- index.mjs
+++ index.mjs
@@ -8,6 +8,10 @@ const PORT = 4500;
 app.use(express.json())
 app.use(cors())
 
+app.get('/api', (req, res) => {
+    res.send("Hello World from server on api route")
+})
+
 app.listen(PORT , ()=>{
     console.log(`listening on port:${PORT}`)
 });
